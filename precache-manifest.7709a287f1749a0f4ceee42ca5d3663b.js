8e7069878f0dda69b8c97200c70e24a384afa9b7
Code Sandbox <hello@codesandbox.io>
2022-02-28 10:22:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7709a287f1749a0f4ceee42ca5d3663b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8946ea0e0f9294e15d54c51dacce97bc",
+    "url": "https://erfan377.github.io/csb-f3t93f/index.html"
+  },
+  {
+    "revision": "5cb67dd126b942d9a958",
+    "url": "https://erfan377.github.io/csb-f3t93f/static/css/main.af70875b.chunk.css"
+  },
+  {
+    "revision": "ffd1ed0f1ba4b97fd658",
+    "url": "https://erfan377.github.io/csb-f3t93f/static/js/2.811a794a.chunk.js"
+  },
+  {
+    "revision": "5cb67dd126b942d9a958",
+    "url": "https://erfan377.github.io/csb-f3t93f/static/js/main.ac5ddb66.chunk.js"
+  },
+  {
+    "revision": "6cc340d23af714816e53",
+    "url": "https://erfan377.github.io/csb-f3t93f/static/js/runtime~main.6a057d00.js"
+  }
+]);
